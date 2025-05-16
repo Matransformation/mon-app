9f6d0786041eb86796fa4a9a940d780de05cb169
Clemenceetromain <contact@clemence-et-romain.fr>
2025-05-16 17:12:15 +0200

    fix: repositionnement et traduction du bouton OneSignal pour éviter chevauchement avec WhatsApp

--- pages/_app.js
+++ pages/_app.js
@@ -68,23 +68,44 @@ export default function App({ Component, pageProps }) {
               defer
             />
             <Script
-              id="onesignal-init"
-              strategy="afterInteractive"
-              dangerouslySetInnerHTML={{
-                __html: `
-                  window.OneSignalDeferred = window.OneSignalDeferred || [];
-                  OneSignalDeferred.push(async function(OneSignal) {
-                    await OneSignal.init({
-                      appId: "e05dcce1-435a-4585-a0bb-80a877ad05f7",
-                      safari_web_id: "web.onesignal.auto.57daeefd-2777-4d55-aef6-93b3ff4b973a",
-                      notifyButton: {
-                        enable: true,
-                      },
-                    });
-                  });
-                `,
-              }}
-            />
+  src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js"
+  strategy="afterInteractive"
+  defer
+/>
+<Script
+  id="onesignal-init"
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{
+    __html: `
+      window.OneSignalDeferred = window.OneSignalDeferred || [];
+      OneSignalDeferred.push(async function(OneSignal) {
+        await OneSignal.init({
+          appId: "e05dcce1-435a-4585-a0bb-80a877ad05f7",
+          safari_web_id: "web.onesignal.auto.57daeefd-2777-4d55-aef6-93b3ff4b973a",
+          notifyButton: {
+            enable: true,
+            position: 'top-right',
+            text: {
+              'tip.state.unsubscribed': 'Recevoir les notifications',
+              'tip.state.subscribed': 'Vous êtes abonné',
+              'tip.state.blocked': 'Notifications bloquées',
+              'message.prenotify': "Cliquez pour activer les notifications",
+              'message.action.subscribed': "Merci de vous être abonné !",
+              'message.action.resubscribed': "Vous êtes à nouveau abonné",
+              'message.action.unsubscribed': "Vous ne recevrez plus de notifications",
+              'dialog.main.title': "Gérer les notifications",
+              'dialog.main.button.subscribe': "S’abonner",
+              'dialog.main.button.unsubscribe': "Se désabonner",
+              'dialog.blocked.title': "Débloquez les notifications",
+              'dialog.blocked.message': "Suivez ces instructions pour autoriser les notifications :"
+            }
+          }
+        });
+      });
+    `,
+  }}
+/>
+
           </>
         )}
 
