e0b3c83cfb897e2f290996d2df9b20e62406ba00
Clemenceetromain <contact@clemence-et-romain.fr>
2025-05-16 14:56:23 +0200

    Ajout du Facebook Pixel dans _app.js

--- _app.js
+++ _app.js
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }) {
   return (
     <SessionProvider session={pageProps.session}>
       <>
-        {/* Google Analytics GA4 */}
+        {/* Google Analytics */}
         <Script
           strategy="afterInteractive"
           src="https://www.googletagmanager.com/gtag/js?id=G-J3JHVGXW4Z"
@@ -25,7 +25,37 @@ export default function App({ Component, pageProps }) {
             `,
           }}
         />
-        
+
+        {/* Meta Pixel */}
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '582481197693399');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+
+        {/* Pixel fallback (for noscript users) */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=582481197693399&ev=PageView&noscript=1"
+          />
+        </noscript>
+
         <Component {...pageProps} />
         <SpeedInsights />
       </>
