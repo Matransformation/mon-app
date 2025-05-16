9a3357f27f7f17be81593c4132d7270416788926
Clemenceetromain <contact@clemence-et-romain.fr>
2025-05-16 15:50:44 +0200

    Ajout des balises noscript pour Meta Pixel et Google Analytics

--- _document.js
+++ _document.js
@@ -10,8 +10,29 @@ export default class MyDocument extends Document {
           <noscript id="__next_css__DO_NOT_USE__" />
         </Head>
         <body>
-          <Main/>
-          <NextScript/>
+          <Main />
+          <NextScript />
+
+          {/* Facebook Pixel noscript fallback */}
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src="https://www.facebook.com/tr?id=582481197693399&ev=PageView&noscript=1"
+              alt="fb-pixel"
+            />
+          </noscript>
+
+          {/* Google Analytics noscript fallback */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=G-J3JHVGXW4Z"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
         </body>
       </Html>
     )
