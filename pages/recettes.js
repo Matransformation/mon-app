120e3bf1a1ec4cb8380f13affa6c220503fe7843
Clemenceetromain <contact@clemence-et-romain.fr>
2025-05-26 11:51:58 +0200

    Amélioration de la home : images Cloudinary + marges mobile

--- pages/recettes.js
+++ pages/recettes.js
@@ -103,7 +103,7 @@ function ListeRecettes() {
   return (
     <>
       <Navbar />
-      <div className="w-full px-4 sm:px-6 lg:px-8 py-6">
+      <div className="max-w-7xl mx-auto px-6 py-6">
         <h1 className="text-3xl font-bold mb-6 text-center">🍴 Toutes nos Recettes</h1>
 
         {/* Barre recherche */}
