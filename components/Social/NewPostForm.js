f3f59168c1b655bcdbba3e501ec9cc829057f878
Clemenceetromain <contact@clemence-et-romain.fr>
2025-05-27 13:55:26 +0200

    Ajout de témoignages, upload Cloudinary et corrections

--- components/Social/NewPostForm.js
+++ components/Social/NewPostForm.js
@@ -14,7 +14,6 @@ export default function NewPostForm({ authorId, onPostCreated }) {
     const formData = new FormData();
     formData.append("file", imageFile);
     formData.append("upload_preset", process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET);
-    formData.append("cloud_name", process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME);
 
     const res = await fetch(
       `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`,
@@ -25,6 +24,11 @@ export default function NewPostForm({ authorId, onPostCreated }) {
     );
 
     const data = await res.json();
+
+    if (!data.secure_url) {
+      throw new Error("Échec de l’upload Cloudinary");
+    }
+
     return data.secure_url;
   };
 
